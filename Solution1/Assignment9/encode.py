d3edf1d4ad562a78f496073c4d73210c8fec83cc
Johannes Benjamin Wendrich <wendrichj@bioclient8.bio.ifi.lmu.de>
2026-02-24 20:44:21 +0100

    encode.py

--- Solution1/Assignment9/encode.py
+++ Solution1/Assignment9/encode.py
@@ -13,16 +13,23 @@ Zelllinien = {}
 RNAseqSet = set()
 ChipseqSet = set()
 with open (inputt, "r") as f:
+    next(f)
     for line in f:
         line = line.strip()
+        if not line:
+            continue
         spalten = line.split(",")
-        Datatype = spalten[0]
-        Zelllinie = spalten[1]
+        Datatype = spalten[0].strip()
+        Zelllinie = spalten[1].strip()
         if "=" in spalten[2]:
 
-            Antibody = spalten[2].split("=")[1]
+            Antibody = spalten[2].split("=")[1].strip()
+            Antibody = Antibody.strip()
+            Antibody = Antibody.split()[0]
+            Antibody = Antibody.strip()
         else:
             Antibody = None
+
         DCCNummer = spalten[9]
         if Datatype in DataTypes:
             DataTypes[Datatype] += 1
@@ -31,8 +38,8 @@ with open (inputt, "r") as f:
 
         if Zelllinie not in Zelllinien:
             Zelllinien[Zelllinie] = set()
-
-        if Datatype == "ChIP-seq":
+#and Antibody != "Input"
+        if Datatype == "ChIP-seq" and Antibody:
             Zelllinien[Zelllinie].add(Antibody)
 
         #für dritten Teil der Aufgabe
@@ -45,20 +52,36 @@ with open (inputt, "r") as f:
 
 RNAundChIp = RNAseqSet & ChipseqSet
 RNAundChIPDCC = {}
-
+DCCNummern = []
+#seen = set()
 with open (inputt, "r") as f:
+    next(f)
     for line in f:
         line = line.strip()
+        if not line:
+            continue
         spalten = line.split(",")
-        Datatype = spalten[0]
-        Zelllinie = spalten[1]
+        Datatype = spalten[0].strip()
+        Zelllinie = spalten[1].strip()
         if "=" in spalten[2]:
 
-            Antibody = spalten[2].split("=")[1]
+            Antibody = spalten[2].split("=")[1].strip()
+            Antibody = Antibody.strip()
+            Antibody = Antibody.split()[0]
+            Antibody = Antibody.strip()
         else:
             Antibody = None
 
-        DCCNummer = spalten[9]
+
+        DCCNummer = spalten[9].strip()
+        if DCCNummer not in DCCNummern:
+            DCCNummern.append(DCCNummer)
+
+        key = (Zelllinie, Datatype, DCCNummer)
+
+        #if key in seen:
+         #   continue
+        #seen.add(key)
 
         if Zelllinie in RNAundChIp and (
         Datatype == "RNA-seq" or
@@ -66,8 +89,15 @@ with open (inputt, "r") as f:
             ):
             if DCCNummer:
                 if Zelllinie not in RNAundChIPDCC:
-                    RNAundChIPDCC[Zelllinie] = set()
-                RNAundChIPDCC[Zelllinie].add(DCCNummer)
+                    RNAundChIPDCC[Zelllinie] = {
+                        "RNA-seq": [],
+                        "ChIP-seq": []
+                    }
+
+                if Datatype == "RNA-seq":
+                    RNAundChIPDCC[Zelllinie]["RNA-seq"].append(DCCNummer)
+                if Datatype == "ChIP-seq" and Antibody == "H3K27me3":
+                    RNAundChIPDCC[Zelllinie]["ChIP-seq"].append(DCCNummer)
 
 exptypes_path = os.path.join(outputFolder, "exptypes.tsv")
 antibodies_path = os.path.join(outputFolder, "antibodies.tsv")
@@ -83,20 +113,11 @@ with open(antibodies_path, "w") as f:
         f.write(zelllinie + "\t" + str(len(antibodies)) + "\n")
 
 with open(chip_path, "w") as f:
-    for key, values in RNAundChIPDCC.items():
-        for v in sorted(values):
-            f.write(key + "\t" + str(v) + "\n")
+    f.write("cell line\tRNAseq Accession\tChIPseq Accession\n")
+    for Zelllinie in sorted (RNAundChIPDCC):
 
-
-
-
-
-
-
-
-
-
-
-
+        rnaDCC = ",".join(sorted(RNAundChIPDCC[Zelllinie]["RNA-seq"]))
+        chipDCC = ",".join(sorted(RNAundChIPDCC[Zelllinie]["ChIP-seq"]))
+        f.write(f"{Zelllinie}\t{rnaDCC}\t{chipDCC}\n")
 
 
