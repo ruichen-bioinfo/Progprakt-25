470e2eabd2482650477fdeb397e6268ae0887aa9
Rui Chen <chenru@bioclient8.bio.ifi.lmu.de>
2026-03-04 11:15:29 +0100

    update

--- Solution3/GORProject/Train.java
+++ Solution3/GORProject/Train.java
@@ -3,14 +3,13 @@ import java.util.*;
 
 public class Train {
     private static final String AA_ORDER = "ACDEFGHIKLMNPQRSTVWY";
-    private static final String SS_ORDER = "HEC"; // Query H, E, C
+    private static final String SS_ORDER = "CEH"; // order: Coil, Extended, Helix
     private static final int WINDOW = 17;
     private static final int CENTER = 8;
 
     // counts[centerAA][state][neighborAA][windowPos]
-    // Even for GOR I/III，Full stack first，save mod after method I/III/IV
     private long[][][][] counts = new long[20][3][20][WINDOW];
-    private long[] stateCounts = new long[3]; // H, E, C total count
+    private long[] stateCounts = new long[3]; // CEH total count
 
     public static void main(String[] args) {
         String dbPath = null;
@@ -59,7 +58,6 @@ public class Train {
                     else if (inSs) ssBuilder.append(line.trim());
                 }
             }
-            // Process last entry
             if (seqBuilder.length() > 0 && ssBuilder.length() > 0) {
                 processPair(seqBuilder.toString(), ssBuilder.toString(), method);
             }
@@ -74,36 +72,43 @@ public class Train {
     }
 
     private void processPair(String seq, String ss, String method) {
-        // Remove spaces inside sequence if any (just in case)
         seq = seq.replaceAll("\\s+", "");
         ss = ss.replaceAll("\\s+", "");
-
         if (seq.length() != ss.length()) return;
 
         int len = seq.length();
         for (int i = 0; i < len; i++) {
             char cAA = seq.charAt(i);
             int cAAidx = AA_ORDER.indexOf(cAA);
-            if (cAAidx == -1) continue; // Skip B, Z, X
+            if (cAAidx == -1) continue;
 
             char cSS = ss.charAt(i);
-            int cSSidx = 2; // Default to Coil
+            int cSSidx = 2; // default Coil
+            if (cSS == 'H' || cSS == 'G' || cSS == 'I') cSSidx = 0; // Helix -> index 0 in CEH? Wait: CEH: 0=C,1=E,2=H. So Helix should be 2, not 0. Correction needed!
+            // Let's re-evaluate: SS_ORDER = "CEH" -> 0:C, 1:E, 2:H.
+            // So mapping: H/G/I -> 2 (Helix), E/B -> 1 (Sheet), others -> 0 (Coil).
+            // Fix mapping below.
+            // Actually in your code you have cSSidx = 2 default, then if H/G/I set to 0? That's wrong.
+            // Let's correct:
 
-            // Coil Mapping Logic
-            if (cSS == 'H' || cSS == 'G' || cSS == 'I') cSSidx = 0;
-            else if (cSS == 'E' || cSS == 'B') cSSidx = 1;
-            // Else (C, S, T, -, space) -> 2 (Coil)
+            int stateIdx;
+            if (cSS == 'H' || cSS == 'G' || cSS == 'I') {
+                stateIdx = 2; // Helix
+            } else if (cSS == 'E' || cSS == 'B') {
+                stateIdx = 1; // Sheet
+            } else {
+                stateIdx = 0; // Coil (including C, T, S, -)
+            }
 
-            stateCounts[cSSidx]++;
+            stateCounts[stateIdx]++;
 
-            // Fill counts
             for (int w = 0; w < WINDOW; w++) {
                 int p = i + (w - CENTER);
                 if (p >= 0 && p < len) {
                     char nAA = seq.charAt(p);
                     int nAAidx = AA_ORDER.indexOf(nAA);
                     if (nAAidx != -1) {
-                        counts[cAAidx][cSSidx][nAAidx][w]++;
+                        counts[cAAidx][stateIdx][nAAidx][w]++;
                     }
                 }
             }
@@ -112,37 +117,13 @@ public class Train {
 
     private void saveModel(String path, long totalValid, String method) throws IOException {
         try (PrintWriter pw = new PrintWriter(path)) {
-            pw.println("METHOD=" + method);
-            pw.println("TOTAL=" + totalValid);
-            pw.println("PRIORS=" + stateCounts[0] + "," + stateCounts[1] + "," + stateCounts[2]);
-            pw.println();
+            // No METHOD, TOTAL, PRIORS lines
 
-            // GOR I and GOR III use the Flat Format (# H ...)
-            // GOR IV usually uses Matrix4D, but if the reference is Flat, we use Flat.
-            // Based on your paste, GOR1 and GOR3 are definitely Flat.
+            if (method.equals("gor1")) {
+                pw.println("// Matrix3D");
+                pw.println();
 
-            if (method.equals("gor4")) {
-
-                 pw.println("// Matrix4D");
-                 pw.println();
-                 for (int cAA = 0; cAA < 20; cAA++) {
-                    for (int s = 0; s < 3; s++) {
-                        pw.println("=" + AA_ORDER.charAt(cAA) + "," + SS_ORDER.charAt(s) + "=");
-                        pw.println();
-                        for (int nAA = 0; nAA < 20; nAA++) {
-                            pw.print(AA_ORDER.charAt(nAA));
-                            for (int w = 0; w < WINDOW; w++) {
-                                pw.print("\t" + counts[cAA][s][nAA][w]);
-                            }
-                            pw.println();
-                        }
-                        pw.println();
-                    }
-                }
-            } else {
-                // GOR I and GOR III -> Flat Format
-
-                // 1. Marginalize (Sum over CenterAA)
+                // Marginalize over centerAA
                 long[][][] flatCounts = new long[3][20][WINDOW];
                 for (int cAA = 0; cAA < 20; cAA++) {
                     for (int s = 0; s < 3; s++) {
@@ -154,25 +135,36 @@ public class Train {
                     }
                 }
 
-                // 2. Output
                 for (int s = 0; s < 3; s++) {
-                    pw.println("# " + SS_ORDER.charAt(s));
+                    pw.println("=" + SS_ORDER.charAt(s) + "=");
+                    pw.println("\t"); // tab line as in reference
                     for (int nAA = 0; nAA < 20; nAA++) {
-                        pw.print(AA_ORDER.charAt(nAA)); // AA at start of line
-
+                        pw.print(AA_ORDER.charAt(nAA));
                         for (int w = 0; w < WINDOW; w++) {
-                            long val = flatCounts[s][nAA][w];
-
-                            // GOR I Special Handling: Only center (index 8) has value
-                            if (method.equals("gor1")) {
-                                if (w != CENTER) val = 0;
-                            }
-
-                            pw.print("\t" + val); // Tab separated integers
+                            pw.print("\t" + flatCounts[s][nAA][w]);
                         }
                         pw.println();
                     }
                     pw.println();
+                }
+            } else {
+                // gor3, gor4: Matrix4D format
+                pw.println("// Matrix4D");
+                pw.println();
+
+                for (int cAA = 0; cAA < 20; cAA++) {
+                    for (int s = 0; s < 3; s++) {
+                        pw.println("=" + AA_ORDER.charAt(cAA) + "," + SS_ORDER.charAt(s) + "=");
+                        pw.println("\t");
+                        for (int nAA = 0; nAA < 20; nAA++) {
+                            pw.print(AA_ORDER.charAt(nAA));
+                            for (int w = 0; w < WINDOW; w++) {
+                                pw.print("\t" + counts[cAA][s][nAA][w]);
+                            }
+                            pw.println();
+                        }
+                        pw.println();
+                    }
                 }
             }
         }
