b20eff1c9ab2121892dee18d91872171a51d5549
Johannes <wendrichj@cip.ifi.lmu.de>
2026-03-04 16:16:26 +0100

    paar Fixes

--- Solution3/Alignments/untitled/src/NeedlemanWunschFreeshift.java
+++ Solution3/Alignments/untitled/src/NeedlemanWunschFreeshift.java
@@ -1,5 +1,20 @@
 public class NeedlemanWunschFreeshift extends NeedlemanWunsch {
-    public NeedlemanWunschFreeshift(Sequence seq1, Sequence seq2, Matrix scoringMatrix) {
-        super(seq1, seq2, scoringMatrix);
+    public NeedlemanWunschFreeshift(Sequence seq1, Sequence seq2, Matrix scoringMatrix, GapPenalty gapPenalty) {
+        super(seq1, seq2, scoringMatrix, gapPenalty);
+    }
+
+    @Override
+    protected void initMatrix() {
+
+    }
+
+    @Override
+    protected void traceback() {
+
+    }
+
+    @Override
+    protected int computeCellValue(int i, int j) {
+        return 0;
     }
 }
