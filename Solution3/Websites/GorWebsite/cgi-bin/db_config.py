7f2a36f1f1eb88df16ca63fdcfa6173d0b0c87b0
Rui Chen <chenru@cip.ifi.lmu.de>
2026-03-07 11:59:13 +0100

    Add new file

---
+++ Solution3/Websites/GorWebsite/cgi-bin/db_config.py
@@ -0,0 +1,9 @@
+
+DB_CONFIG = {
+    'user': 'bioprakt11',
+    'password': '$1$hpHspKtX$b6TtLHRsabtwlXnc.muv60',
+    'host': 'mysql2-ext.bio.ifi.lmu.de',
+    'port': 3306,
+    'database': 'bioprakt11',
+    'raise_on_warnings': True
+}
